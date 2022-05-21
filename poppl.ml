ef8ae806d7541480bd3b218c9f7cba30139503f4
nicoszpako <nicoszpako@aol.com>
2022-05-22 01:14:00 +0200

    Quite working well

--- poppl.ml
+++ poppl.ml
@@ -74,10 +74,10 @@ let rec get_variable_value name evaluation_context = match evaluation_context wi
 
 (* --- Log handling --- *)
 
-(* Retrieve the newest time message in order to get the actual system time *)
+(* Retrieve the newest time message in order to get the current system time *)
 let rec now (log : e) = match log with
 	| Log([]) -> 0.
-	| Log((a,_,time)::q) when a = "time_message" -> time
+	| Log((a,[Float(time)],_)::q) when a = "time_message" -> time
 	| Log(a::q) -> now (Log(q))
 ;;
 
@@ -87,9 +87,22 @@ let log_length (log : e) = match log with
 ;;
 
 
+(* --- Printing utils --- *)
+
+let rec string_of_handler_list handler_list = match handler_list with
+	| [] -> ""
+	| (name,f)::q -> "["^name^"] "^(string_of_handler_list q)
+;;
+
+let rec string_of_log log = match log with
+	| Log([]) -> ""
+	| Log((name,data,time)::q) -> "["^name^" at "^(string_of_float time)^"] "^(string_of_log (Log(q)))
+;;
+
+
 (* --- Handlers handling --- *)
 
-(* Adds the (name,h) handler to the given handler_list *)
+(* Adds the (name,h) handler to the given handler_list. *)
 let add_handler (name : name) (h : e -> e) (handler_list : handler list) =
 	let rec browse l = match l with
 		| [] -> (name,h)::handler_list
@@ -97,7 +110,7 @@ let add_handler (name : name) (h : e -> e) (handler_list : handler list) =
 	browse handler_list
 ;;
 
-(* Adds the (name,e) handler to the given handler_list *)
+(* Adds the (name,e) handler to the given handler_list. *)
 let remove_handler (name : name) (handler_list : handler list) =
 	let rec browse l = match l with
 		| [] -> l
@@ -105,11 +118,23 @@ let remove_handler (name : name) (handler_list : handler list) =
 	browse handler_list
 ;;
 
+(* Finds the latest message with the given identifier in the log. *)
+(* log : The current log *)
+(* n : The wanted message identifier to be found *)
+(* Returns : A log containing the one wanted message, or Void if no message has been found *)
 let rec find_message (log : e) (n : string) = match log with
 	| Log([]) -> Void
 	| Log((s,d,time)::q) -> if s = n then (Log([(s,d,time)])) else find_message (Log(q)) n
 ;;
 
+
+(* Evaluates the given expression and returns the next state. *)
+(* e : The expression to evaluate *)
+(* handler_list : The current handler list *)
+(* log : The current log *)
+(* outgoing_messages : The current outgoing messages set*)
+(* evaluation_context : The current context storing the current set of variables *)
+(* Returns : a 4-uplet (next_expression_to_evaluate, new_handler_list, new_outgoing_messages_set, new_context) *)
 let rec eval (e : e) (handler_list : handler list) (log : e) (outgoing_messages : e) (evaluation_context : context) = 
 	let rec eval_native (o : o) (params : e list) (handler_list : handler list) (log : e) (outgoing_messages : e) (evaluation_context : context) = 
 		let Log(l) = log in
@@ -182,8 +207,8 @@ let rec eval (e : e) (handler_list : handler list) (log : e) (outgoing_messages 
 			end
 		| Time_passed -> begin match params with
 			| [] 													-> 	Void
-			| [Void;_;_]											-> 	True
-			| [Float(actual_time);Float(sent_time);Float(delay)] 	-> 	if sent_time +. delay < actual_time then True else False
+			| [_;Void;_]											-> 	True
+			| [Float(current_time);Float(start_time);Float(delay)] 	-> 	if start_time +. delay <= current_time then True else False
 			| [c;a;b] 												-> 	let (v,_,_,_) = eval c handler_list log outgoing_messages evaluation_context in
 																		let (s,_,_,_) = eval a handler_list log outgoing_messages evaluation_context in
 																		let (f,_,_,_) = eval b handler_list log outgoing_messages evaluation_context in
@@ -194,7 +219,7 @@ let rec eval (e : e) (handler_list : handler list) (log : e) (outgoing_messages 
 	match e with
 	| Void | True | False | Float(_) | String(_) | Log(_) -> (e, handler_list, outgoing_messages, evaluation_context)
 	| Variable(name) -> (get_variable_value name evaluation_context, handler_list, log, evaluation_context)
-	| Send(n, d) -> eval (Void) handler_list log (Log(m@[(n,d,now log)])) evaluation_context (* Appending at the end of the list in order to implement a Queue-like structure *)
+	| Send(n, d) -> eval (Void) handler_list log (Log((n,d,now log)::m)) evaluation_context
 	| Add(name,e) -> eval (Void) (add_handler name e handler_list) log outgoing_messages evaluation_context
 	| Remove(name) -> eval (Void) (remove_handler (name) handler_list) log outgoing_messages evaluation_context
 	| Begin(List(l)) -> begin let rec browse_list l handler_list log outgoing_messages evaluation_context = match l with
@@ -212,7 +237,13 @@ let rec eval (e : e) (handler_list : handler list) (log : e) (outgoing_messages 
 
 ;;
 
-(* Evaluates one handler, and returns the next set of handler H and the outgoing messages *) 
+(* Evaluates one handler, and returns the next set of handler H and the outgoing messages *)
+(* handler : The handler to evaluate *)
+(* handler_list : The current handler list *)
+(* log : The current log *)
+(* outgoing_messages : The current outgoing messages set *)
+(* evaluation_context : The current context storing the current set of variables *)
+(* Returns : a 4-uplet (Void, new_handler_list, new_outgoing_messages_set, new_context) *)
 let eval_handler (handler : handler) (handler_list : handler list) (log : e) (outgoing_messages : e) (evaluation_context : context) = 
 	let (_,f) = handler in
 	eval (f log) handler_list log outgoing_messages evaluation_context
@@ -226,22 +257,57 @@ let browse_handlers (h : handler list) (log : e) (evaluation_context : context) 
 		| [] -> (h_f, outgoing_messages, evaluation_context)
 		| a::q -> let (_, h_f, outgoing_messages, evaluation_context) = eval_handler a h_f log outgoing_messages evaluation_context in
 						 build_future_handler_set q h_f log outgoing_messages evaluation_context
-	in build_future_handler_set h h log (Log([])) evaluation_context (* We build the future handler set based on the actual one, with an empty starting outgoing messages list *)
+	in build_future_handler_set h h log (Log([])) evaluation_context (* We build the future handler set based on the current one, with an empty starting outgoing messages list *)
 ;;
 
 (* Looping over outgoing messages, and for each one we trigger all the handlers *)
-let rec browse_outgoing_messages (h : handler list) (log : e) (outgoing_messages : e) (evaluation_context : context) = 
+(* handler_list : The current handler list *)
+(* log : The current log *)
+(* outgoing_messages : The current outgoing messages set *)
+(* evaluation_context : The current context storing the current set of variables *)
+(* Returns : a 3-uplet (new_handler_list, new_log, new_context) *)
+let rec browse_outgoing_messages (handler_list : handler list) (log : e) (outgoing_messages : e) (evaluation_context : context) = 
+	(* print_endline ("browsing outgoing messages with handler list : "^(string_of_handler_list handler_list)^" and log : "^(string_of_log log)); *)
 	let Log(l) = log in
 	match outgoing_messages with
-	| Log([]) -> (h, log, evaluation_context)
-	| Log(a::q) -> let (h_f, Log(outgoing_messages_to_add), evaluation_context) = browse_handlers h (Log(a::l)) evaluation_context in
+	| Log([]) -> (handler_list, log, evaluation_context)
+	| Log(a::q) -> let (h_f, Log(outgoing_messages_to_add), evaluation_context) = browse_handlers handler_list (Log(a::l)) evaluation_context in
 				browse_outgoing_messages h_f (Log(a::l)) (Log(q@outgoing_messages_to_add)) evaluation_context
 ;;
 
+(* Runs once over a handler set, from scratch. *)
+(* h0 : The starting handler set *)
+(* Returns : a 3-uplet (new_handler_list, new_log, new_context) *)
 let run (h0 : handler list) =  
 	browse_outgoing_messages h0 (Log([])) (Log([("initialisation",[],0.)])) []
 ;;
 
+let start (h0 : handler list) =
+	let h = ref h0 in
+	let log = ref (Log([])) in
+	let context = ref [] in
+	let outgoing_messages = ref (Log([("initialisation",[],0.)])) in
+	let stopped = ref false in
+	print_endline ("starting handler list : "^(string_of_handler_list h0));
+	while not !stopped do
+		let (nh, nl, nc) = browse_outgoing_messages (!h) (!log) (!outgoing_messages) (!context) in
+		h:=nh;
+		log:=nl;
+		context:=nc;
+		outgoing_messages:=(Log([]));
+		print_endline ("handler list : "^(string_of_handler_list nh));
+		print_endline ("log : "^(string_of_log nl));
+		print_endline ("");
+		print_string ("> ");
+		let input = read_line () in 
+		print_endline ("");
+		let parameters = String.split_on_char ' ' input in match parameters with
+		| ["wait";t] ->	let time = float_of_string t in
+						let Log(l) = (!outgoing_messages) in outgoing_messages := (Log([("time_message",[Float((now !log) +. time)],(now !log) +. time)]))
+		| ["stop"] ->	stopped:= true	 	
+		| _ -> ()
+	done
+;;
 
 (* --- Syntax shortcuts --- *)
 
@@ -255,26 +321,28 @@ let whenever_last_messages_in s a b body = If(Native(Is_in,[Native(Message_paylo
 let whenever_last_messages_outside_of s a b body = If(Native(Not,[Native(Is_in,[Native(Message_payload,[Native(Most_recent,[String(s)])]);a;b])]),body,Void);; 
 
 (* After instruction *)
+(* time : The time after wich the given body has to be triggered *)
+(* body : The expression evaluated when the condition is verified *)
+(* log : The current log *)
 let after (time : e) (body : e) (log : e) =
 	let n = "after_"^(string_of_int (log_length log)) in 
 	Add(n,
 		fun log -> If(
-			Native(Time_passed,[Native(Time_of,[Native(Most_recent,[])]);Float(now log);time]),
+			Native(Time_passed,[Float(now log);Native(Time_of,[Native(Most_recent,[])]);time]),
 			Begin(List([body; Remove(n)])),
 			Void
 		)
 	)
 ;;
 
-
 (* Every instruction *)
 (* delay : Duration from which a new message can be sent *)
 (* body : The expression evaluated when the condition is verified *)
 (* message : The message (identifier,data) that is used to get the latest date at wich it was sent in the log *)
-(* log : The actual log *)
+(* log : The current log *)
 let every (delay : float) (body : e) message (log : e) =
 	let (s,d) = message in
-	If(Native(Time_passed, [Native(Time_of,[Native(Most_recent,[String(s)])]);Float(now log);Float(delay)]),
+	If(Native(Time_passed, [Float(now log);Native(Time_of,[Native(Most_recent,[String(s)])]);Float(delay)]),
 		body,
 		Void
 	)
